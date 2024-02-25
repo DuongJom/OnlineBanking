d3a590d2c93c164566fcd65e58b64a58e9f97943
DuongJom <ngocduong5642@gmail.com>
2024-02-25 00:23:18 -0500

    config tailwind css for project

--- OnlineBanking/controllers/home_controller.py
+++ OnlineBanking/controllers/home_controller.py
@@ -5,3 +5,7 @@ home_blueprint = Blueprint('home', __name__)
 @home_blueprint.route('/')
 def index():
     return render_template('home.html')
+
+@home_blueprint.route('/about')
+def about():
+    return render_template('about.html')
