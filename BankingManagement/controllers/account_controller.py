a9e201e448d012a8811c8212507501fb1afa0964
Jake6868 <congnvpyvp@gmail.com>
2024-03-23 11:18:37 +0800

    Revert "forgot password-first commit"
    This reverts commit 51572a82374024bdc83df2c668c512c6a4da6718.

--- BankingManagement/controllers/account_controller.py
+++ BankingManagement/controllers/account_controller.py
@@ -1,26 +1,17 @@
-from flask import (Blueprint, render_template, request, redirect, url_for, flash)
+from flask import Blueprint, render_template, request, redirect, url_for, flash
+
 from models import account, database
 
 db = database.Database().get_db()
 collection = db['accounts']
 account_blueprint = Blueprint('account', __name__)
 
-@account_blueprint.route('/login', methods=['GET', 'POST'])
-def login():
-    if request.method == 'POST':
-        pass
-    return render_template('login.html')
-
-@account_blueprint.route('/logout', methods=['POST'])
-def logout():
-    pass
-
 @account_blueprint.route('/register', methods=['GET','POST'])
 def register():
     if request.method == 'POST':
         # get the data from post request
         email = request.form['email']
-        print(email)
+        username = request.form['username']
         password = request.form['password']
         confirmPassword = request.form['confirmPassword']
 
@@ -28,21 +19,24 @@ def register():
         error = None
         if not email:
             error = 'email is required.'
+        elif not username:
+            error = 'username is required.'
         elif not password:
             error = 'Password is required.'
         elif password != confirmPassword:
             error = 'Confirmation password does not match'
         # check if email already exist
         existEmail = collection.find_one({"email": email})
-        print(existEmail)
-        if existEmail != None:
+        existUsername = collection.find_one({"Username": username})
+        if existEmail:
             error = f"Email {email} is already registered." 
+        if existUsername:
+            error = f"Username {username} is already registered." 
         # insert the document to the collection if there is no error
         if error is None:
-            acc = account.Account(email=email, password=password)
+            acc = account.Account(email,password,username)
             collection.insert_one(acc.to_json())
             return redirect(url_for("account.login"))
                 
         flash(error)
     return render_template('register.html')
-
--- BankingManagement/models/account.py
+++ BankingManagement/models/account.py
@@ -1,10 +1,27 @@
-class Account:
-    def __init__(self, email, password):
+from  werkzeug.security import generate_password_hash
+from models.base import BaseModel
+class Account(BaseModel):
+    def __init__(self, email, password, Username, AccountNumber, Branch, AccountOwner, LoginMethod, TransferMethod, Service):
         self.email = email
-        self.password = password
+        self.password = generate_password_hash(password)
+        self.Username = Username
+        self.AccountNumber = AccountNumber
+        self.Branch = Branch
+        self.AccountOwner = AccountOwner
+        self.LoginMethod = LoginMethod or []
+        self.TransferMethod = TransferMethod or []
+        self.Service = Service or []
 
     def to_json(self):
         return {
             "email": self.email,
-            "password": self.password
+            "password": self.password,
+            "Username": self.Username,
+            "AccountNumber": self.AccountNumber,
+            "Branch": self.Branch,
+            "AccountOwner": self.AccountOwner,
+            "LoginMethod": self.LoginMethod,
+            "TransferMethod": self.TransferMethod,
+            "Service": self.Service
         }
+    
