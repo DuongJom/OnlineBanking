97b0d2f6a9ef3c026f58d6e9421ab21971fd66f7
Duong Nguyen <ngocduong5642@gmail.com>
2024-04-18 11:15:27 +0700

    add enums to system enum

--- BankingManagement/SysEnum.py
+++ BankingManagement/SysEnum.py
@@ -13,3 +13,15 @@ class RoleType(Enum):
 class CardType(Enum):
     CREDITS=0
     DEBITS = 1
+
+class WorkingType(Enum):
+    OFF  = 0
+    WFH = 1
+    WORK_IN_COMPANY = 2
+
+class DayOffType(Enum):
+    HALF_DAY = 0
+    VACATION = 1
+    WEDDING = 2
+    SICK = 3
+    OTHER = 4
