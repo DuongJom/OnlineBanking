147fd60887699af3b6c3d176ae3eb740babb07c3
Duong Nguyen <ngocduong5642@gmail.com>
2024-04-18 11:11:34 +0700

    change name of system enums

--- BankingManagement/SysEnum.py
+++ BankingManagement/SysEnum.py
@@ -1,15 +1,15 @@
 from enum import Enum
 
-class SexEnum(Enum):
+class SexType(Enum):
     MALE = 0
     FEMALE = 1
     OTHER = 2
 
-class RoleEnum(Enum):
+class RoleType(Enum):
     USER = 0
     EMPLOYEE = 1
     ADMIN = 2
 
-class CardTypeEnum(Enum):
+class CardType(Enum):
     CREDITS=0
     DEBITS = 1
