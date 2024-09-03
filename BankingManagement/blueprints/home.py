115b8f7140e5a79753701bb2b3e900752d370d18
Duong Nguyen <ngocduong5642@gmail.com>
2024-09-03 12:10:34 +0700

    design UI-transfer-money

--- BankingManagement/blueprints/home.py
+++ BankingManagement/blueprints/home.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, session, redirect
+from flask import Blueprint, render_template, session, redirect, request
 from bson import ObjectId
 
 from models import database
@@ -17,11 +17,18 @@ def index():
     account = accounts.find_one({"_id": account_id})
     if account:
         if account["Role"] == RoleType.USER.value:
-            return render_template("home.html", account = account)
+            return render_template("/user/home.html", account = account)
         elif account["Role"] == RoleType.EMPLOYEE.value:
             return redirect("/employee/home")
         else:
             return redirect("/admin/account")
     return redirect("/login")
 
-
+@home_blueprint.route("/user/transfer-money", methods=["GET", "POST"])
+@login_required
+def transfer_money():
+    account_id = ObjectId(session.get("account_id"))
+    account = accounts.find_one({"_id": account_id})
+    banks = []
+    if request.method == "GET":
+        return render_template("/user/transfer.html", account=account, banks=banks)
